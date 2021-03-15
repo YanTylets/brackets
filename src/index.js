43fd0a044dcf0735e783daa01061ffe2c01b1138
YanTylets <72673268+YanTylets@users.noreply.github.com>
2021-03-15 03:49:21 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = str.split('');
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (arr[i] == bracketsConfig[j][1] && arr[i-1] == bracketsConfig[j][0]) {
+                arr.splice(i-1, 2);
+                i = 0;
+                break;          
+            }
+        } 
+    }
+    return arr < 1 ? true : false
 }
